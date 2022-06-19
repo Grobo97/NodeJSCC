1309080a912e548865444c9b6cc153a747c0fff6
Grobo97 <groberskix@o2.pl>
2022-06-19 15:10:22 +0200

    CC finish

---
+++ index.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req,res) => {
+    if(req.url === '/'){
+        fs.readFile(path.join(__dirname, 'public','index.html'), 
+        (err,content) =>
+        {
+            if (err){
+                res.end(err.code);
+            };
+            res.writeHead(200, { 'Content-Type': 'text/html'});
+            res.end(content);
+        });
+
+        
+    }else if (req.url === '/about'){
+        fs.readFile(path.join(__dirname, 'public','about.html'), 
+        (err,content) =>
+        {
+            if (err){
+                res.end(err.code);
+            };
+            res.writeHead(200, { 'Content-Type': 'text/html'});
+            res.end(content);
+        });
+    }
+});
+const PORT = process.env.PORT || 5000; 
+server.listen(PORT, () => console.log(`Server is running on ${PORT}...`));
+
+
